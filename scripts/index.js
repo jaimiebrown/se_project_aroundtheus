0cb321fc1f951186a38fa860fce98093c7404bb4
Jaimie Brown <jaimiegrayson@gmail.com>
2023-04-08 19:18:44 -0600

    Project 4 Stage 1

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let initialCards = [
+  {
+    name: "Budapest",
+    link: "https://unsplash.com/photos/ezUDjYy17EA)",
+  },
+
+  {
+    name: "Barcelona",
+    link: "https://unsplash.com/photos/LSscVPEyQpI)",
+  },
+
+  {
+    name: "Bruges",
+    link: "https://unsplash.com/photos/0zOkEQ8LRMs",
+  },
+
+  {
+    name: "Prague",
+    link: "https://unsplash.com/photos/oK0PIhO2RPg",
+  },
+
+  {
+    name: "London",
+    link: "https://unsplash.com/photos/mOEqOtmuPG8",
+  },
+
+  {
+    name: "Edinburgh",
+    link: "https://unsplash.com/photos/8g6F-8xdszQ",
+  },
+];
